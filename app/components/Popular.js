7ac68ed1a344ac704cb503fe47171ff943416f40
Christopher Ksiazak <cksiazak@gmail.com>
2019-09-04 23:58:27 -0600

    added language selector and colors to nav using setState

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 export default class Popular extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    });
+  }
+
   render() {
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
 
@@ -8,7 +24,13 @@ export default class Popular extends Component {
       <ul className="flex-center">
         {languages.map(language => (
           <li key={language}>
-            <button className="btn-clear nav-link">{language}</button>
+            <button
+            style={language === this.state.selectedLanguage ? {color: 'rgb(187, 46, 31)'} : null}
+              className="btn-clear nav-link"
+              onClick={() => this.updateLanguage(language)}
+            >
+              {language}
+            </button>
           </li>
         ))}
       </ul>
