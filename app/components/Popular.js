f422e3f7cf9b3805d8e2939c273fd1857f7a1a20
Christopher Ksiazak <cksiazak@gmail.com>
2019-09-04 23:10:53 -0600

    added list for project nav

---
+++ app/components/Popular.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Popular extends Component {
+  render() {
+    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+    return (
+      <ul className="flex-center">
+        {languages.map(language => (
+          <li key={language}>
+            <button className="btn-clear nav-link">{language}</button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
